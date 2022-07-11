acec81688f017deb86fe9ed42543146dd602e65e
jjeong3513 <kkag8997@naver.com>
2022-07-11 14:46:00 +0900

    revise file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {Routes,Route, BrowserRouter} from 'react-router-dom';
 import Nav from "./components/Nav";
 import ProductList from "./components/ProductList";
 import CartList from "./components/CartList";
+import Login from './components/Login';
+import UserRedirects from './route/UserRedirects';
+
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
           <Routes>
             <Route path='/' element={<ProductList />} />
             <Route path='/cart' element={<CartList />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/product/:id' element={<UserRedirects />} />
           </Routes>
       </BrowserRouter>
     </div>
--- src/store/cart/cartSlice.js
+++ src/store/cart/cartSlice.js
@@ -7,12 +7,19 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addCart: (state,action) => {
-        state = state.push(action.payload) ;
+        const equalItem = state.findIndex(ele=> ele.id === action.payload.id); //값이 object로 오기 때문에 [0]을 붙여주지 않음
+        if(equalItem >= 0){
+          alert('장바구니에 동일한 상품이 있습니다.')
+        } else{
+          state = state.push(action.payload);//'state =' 이건 없어도 됨 
+        }
         action.type="SET_ADD_ITEM"
     },
     deleteCart: (state,action) => {
-        state.splice(action.payload,1);
-        action.type="SET_DELETE_ITEM"
+      console.log(action.payload)
+      const num = state.findIndex((ele)=> ele.id === action.payload[0].id) //배열로 오기 때문에 [0]을 붙여줌
+      state = state.splice(num,1);
+      action.type="SET_DELETE_ITEM"
     },
    
   },
